f80b7c31943f3bad6bb2216fcf38eb9035da7a53
Leslie Romero <149332466+Leslie-Romero@users.noreply.github.com>
2025-10-30 23:09:49 +0000

    Tarea última versión (pendiente de últimas modificaciones)

--- tarea.js
+++ tarea.js
@@ -1,28 +1,150 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FlyControls } from "three/examples/jsm/controls/FlyControls";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // Fuentes
 //https://threejs.org/docs/#manual/en/introduction/Creating-a-scene -->
 //https://r105.threejsfundamentals.org/threejs/lessons/threejs-primitives.html  -->
+
+const loader = new GLTFLoader();
+
+let spaceship;
+
 let scene;
 let camera;
 let renderer;
-let estrella, Planetas = [], Lunas = [];
-let planetColors = [0x205069, 0x008080, 0xe29870, 0x86895d, 0x382c1e, 0xfffacd, 0x565656]
+let estrella,
+  Planetas = [],
+  Lunas = [],
+  Asteroides = [];
+let planetSpeeds = [];
 
 let t0 = 0;
 let timestamp;
 let accglobal = 0.001;
 
-let grid, perfil, plano;
-const MAX_POINTS = 500;
-let raycaster;
-let npuntos;
+let info;
+let pauseButton, cameraButton, followButton, speed1, speed2, speed3;
+let speedConfig = 1.0;
+let modes = [];
+let ogSpeeds = [];
+let speeds = [];
+let flightButton;
+let pause = false;
+let offset = 0;
+let savedTimestamp = 0;
+let mode = 0;
+let camcontrols;
+let flyControls;
+let lastTimestamp;
+
+// Texturas
+const txtSun = new THREE.TextureLoader().load("src/textures/2k_sun.jpg");
+const txtMoon = new THREE.TextureLoader().load("src/textures/2k_moon.jpg");
+const txtSpace = new THREE.TextureLoader().load(
+  "src/textures/galaxy-night-landscape.jpg"
+);
+const txtVolcanic = new THREE.TextureLoader().load(
+  "src/textures/Volcanic.png"
+);
+const txtVenusian = new THREE.TextureLoader().load(
+  "src/textures/Venusian.png"
+);
+const txtEarthDay = new THREE.TextureLoader().load(
+  "src/textures/earthmap1k.jpg"
+);
+const txtEarthNight = new THREE.TextureLoader().load(
+  "src/textures/2k_earth_nightmap.jpg"
+);
+const txtEarthSpec = new THREE.TextureLoader().load(
+  "src/textures/2k_earth_specular_map.tif"
+);
+const txtEarthClouds = new THREE.TextureLoader().load(
+  "src/textures/2k_earth_clouds.jpg"
+);
+const txtEarthBump = new THREE.TextureLoader().load(
+  "src/textures/earthbump1k.jpg"
+);
+const txtEarthAlpha = new THREE.TextureLoader().load(
+  "src/textures/earthcloudmaptrans_invert.jpg"
+);
+const txtSwamp = new THREE.TextureLoader().load("src/textures/Swamp.png");
+const txtMartian = new THREE.TextureLoader().load("src/textures/Martian.png");
+const txtGas1 = new THREE.TextureLoader().load("src/textures/Gaseous1.png");
+const txtGas2 = new THREE.TextureLoader().load("src/textures/Gaseous2.png");
+const txtGas4 = new THREE.TextureLoader().load("src/textures/Gaseous4.png");
 
 init();
 animate();
 
+function createButton(name, isActive, top = "", right = "", bottom = "", left = "", type = "") {
+  let btn = document.createElement("button");
+  btn.innerHTML = name;
+  btn.classList.add("btn");
+  if (isActive) {
+    btn.classList.add("active-btn");
+  }
+  btn.style.top = top;
+  btn.style.bottom = bottom;
+  btn.style.left = left;
+  btn.style.right = right;
+  if (type == "mode"){
+    modes.push(btn);
+  } else if (type == "speed") {
+    speeds.push(btn);
+  }
+  return btn;
+}
+
+function selectButton(button, list) {
+  for (let b of list) {
+    if (b == button) {
+      b.classList.add("active-btn");
+    } else {
+      b.classList.remove("active-btn");
+    }
+  }
+}
+
 function init() {
+  // Texto informacion
+  info = document.createElement("div");
+  info.classList.add("info");
+  info.innerHTML = "Práctica 6 - Leslie Romero";
+  document.body.appendChild(info);
+
+  loader.load('src/mevak_shuttle.glb',
+    function (gltf) {
+      spaceship = gltf.scene;
+      spaceship.scale.set(0.001, 0.001, 0.001);
+      spaceship.position.set(0, -6, 0);
+      scene.add(spaceship);
+    }
+  )
+  
+
+  let textures = [
+    txtSun,
+    txtMoon,
+    txtSpace,
+    txtVolcanic,
+    txtVenusian,
+    txtEarthDay,
+    txtEarthSpec,
+    txtEarthClouds,
+    txtEarthNight,
+    txtSwamp,
+    txtMartian,
+    txtGas1,
+    txtGas2,
+    txtGas4,
+  ];
+
+  for (let texture of textures) {
+    texture.colorSpace = THREE.SRGBColorSpace;
+  }
+
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
     75,
@@ -30,125 +152,243 @@ function init() {
     0.1,
     1000
   );
-  camera.position.set(0, -20, 5);
+  camera.position.set(0, 2, -18);
+
+  scene.background = txtSpace;
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   document.body.appendChild(renderer.domElement);
 
-  let camcontrols = new OrbitControls(camera, renderer.domElement);
+  camcontrols = new OrbitControls(camera, renderer.domElement);
+  camcontrols.enabled = true;
+  flyControls = new FlyControls(camera, renderer.domElement);
+  flyControls.enabled = false;
+  flyControls.dragToLook = true;
+  flyControls.movementSpeed = 0.001;
+  flyControls.rollSpeed = 0.01;
 
-  //Asistente GridHelper
-  grid = new THREE.GridHelper(40, 40);
-  grid.position.set(0, 0, 0);
-  grid.geometry.rotateX(Math.PI / 2);
-  //Desplaza levemente hacia la cámara
-  grid.position.set(0, 0, 0.05);
+  Estrella(2, 0xffff00, txtSun);
+  // Volcánico
+  Planeta(scene, 0.2, 3, 0.4, 0xffffff, 1.0, 1.0, txtVolcanic);
+  // Venusiano
+  Planeta(scene, 0.3, 4, 0.3, 0xffffff, 1.0, 1.0, txtVenusian);
+  // Tierra
+  Planeta(
+    scene,
+    0.4,
+    5,
+    0.5,
+    0xffffff,
+    1.0,
+    1.2,
+    txtEarthDay,
+    txtEarthBump,
+    txtEarthSpec
+  );
+  // Luna
+  Luna(Planetas[2], 0.1, 0.6, 0.4, 0xffffff, Math.PI / 2, txtMoon);
+  // Swamp
+  Planeta(scene, 0.45, 7, 0.35, 0xffffff, 1.1, 1.0, txtSwamp);
+  // Martian
+  Planeta(scene, 0.3, 9, 0.25, 0xffffff, 1.0, 1.0, txtMartian);
+  // Gaseoso 1
+  Planeta(scene, 0.7, 10.5, 0.2, 0xffffff, 1.0, 1.0, txtGas1);
+  // Gaseoso 2
+  Planeta(scene, 0.6, 12, 0.22, 0xffffff, 1.0, 1.0, txtGas2);
+  // Gaseoso 4
+  Planeta(scene, 0.8, 13.5, 0.15, 0xffffff, 1.0, 1.1, txtGas4);
 
-  scene.add(grid);
+  for (let planeta of Planetas) {
+    ogSpeeds.push(planeta.userData.speed);
+  }
 
-  //Creo un plano en z=0 que no muestro para la intersección
-  let geometryp = new THREE.PlaneGeometry(40, 40);
-  let materialp = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    side: THREE.DoubleSide,
-  });
-  plano = new THREE.Mesh(geometryp, materialp);
-  plano.visible = false;
-  scene.add(plano);
+  // Botón de pausa
+  pauseButton = createButton("Pause", false, "30px", "20px");
+  pauseButton.onclick = function () {
+    if (pauseButton.innerHTML == "Pause") {
+      pauseButton.classList.add("active-btn");
+      pauseButton.innerHTML = "Paused";
+      savedTimestamp = Date.now();
+      pause = true;
+    } else if (pauseButton.innerHTML == "Paused") {
+      pauseButton.classList.remove("active-btn");
+      pauseButton.innerHTML = "Pause";
+      pause = false;
+      offset += Date.now() - savedTimestamp;
+    }
+  };
+  document.body.appendChild(pauseButton);
 
-  //Rayo para intersección
-  raycaster = new THREE.Raycaster();
+  // Botón modo camera
+  cameraButton = createButton("Camera Mode", true, "", "", "30px", "30px", "mode");
+  cameraButton.onclick = function() {
+    selectButton(cameraButton, modes)
+    camera.position.set(0, 2, -18);
+    camcontrols.target.set(0, 0, 0);
+    camcontrols.update();
+    camcontrols.enabled = true;
+    flyControls.enabled = false;
+    mode = 0;
+  }
+  document.body.appendChild(cameraButton);
 
-  Estrella(3, 0xffff00);
+  // Botón modo vuelo
+  flightButton = createButton("Fight Mode", false, "", "30px", "30px", "mode");
+  flightButton.onclick = function() {
+    selectButton(flightButton, modes)
+    camera.position.set(estrella.position.x, estrella.position.y+1, estrella.position.z-8);
+    camcontrols.target.set(0, 0, 0);
+    camcontrols.update();
+    spaceship.position.set(camera.position.x, camera.position.y, camera.position.z);
+    camcontrols.enabled = false;
+    flyControls.enabled = true;
+    mode = 1;
+  }
+  document.body.appendChild(flightButton);
 
-  //Manejador de eventos
-  document.addEventListener("mousedown", onDocumentMouseDown);
+  // Follow mode
+  followButton = createButton("Follow Mode", false, "", "", "30px", "45%", "mode");
+  followButton.onclick = function() {
+    selectButton(followButton, modes);
+    camera.position.set(Planetas[2].position.x, Planetas[2].position.y-4, Planetas[2].position.z+2);
+    mode = 2;
+  }
+  document.body.appendChild(followButton);
+
+  // Speed buttons
+  speed1 = createButton("x0.5", false, "30px", "", "", "30px", "speed");
+  speed1.onclick = function() {
+    selectButton(speed1, speeds);
+    speedConfig = 0.5;
+    t0 = Date.now();
+  }
+  document.body.appendChild(speed1);
+  speed2 = createButton("x1.0", true, "30px", "", "", "80px", "speed");
+  speed2.onclick = function() { 
+    selectButton(speed2, speeds);
+    speedConfig = 1.0;
+    t0 = Date.now();
+  }
+  document.body.appendChild(speed2);
+  speed3 = createButton("x1.5", false, "30px", "", "", "130px", "speed");
+  speed3.onclick = function() { 
+    selectButton(speed3, speeds);
+    t0 = Date.now();
+  }
+  document.body.appendChild(speed3);
+
+  // Luz ambiente
+  const Lamb = new THREE.AmbientLight(0xffe4b5, 0.3);
+  scene.add(Lamb);
+
+  // Luz puntual
+  const Lpunt = new THREE.PointLight(0xffe4b5, 2.5, 0, 2);
+  Lpunt.position.set(0, 0, 0);
+  Lpunt.castShadow = true;
+  scene.add(Lpunt);
 
   //Inicio tiempo
   t0 = Date.now();
 }
 
-function onDocumentMouseDown(event) {
-  //Conversión coordenadas del puntero
-  const mouse = {
-    x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
-    y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
-  };
-
-  //Intersección, define rayo
-  raycaster.setFromCamera(mouse, camera);
-
-  // Intersecta
-  const intersects = raycaster.intersectObject(plano);
-
-  if (intersects.length > 0) {
-    console.log("X: " + intersects[0].point.x + "; Y: " + intersects[0].point.y + "; Z: " + intersects[0].point.z)
-    let color = Math.floor(Math.random() * (planetColors.length))
-    let nLunas = Math.floor(Math.random() * 4)
-    Planeta(
-      intersects[0].point.x,
-      intersects[0].point.y,
-      intersects[0].point.z,
-      0.7,                  // Radio
-      planetColors[color],  // Color
-      Math.random(),        // Velocidad
-      1.0,                  // f1
-      1.0                   // f2
-    );
-
-    for (let i = 0; i<nLunas; i++){
-      Luna(Planetas[Planetas.length-1], 0.15, 0.9, (i+1)*2, 0xffffff, 0.5*i)
-    }
-
+function Estrella(rad, col, texture = undefined) {
+  let geometry = new THREE.SphereGeometry(rad, 30, 30);
+  let material = new THREE.MeshBasicMaterial({ color: col });
+  if (texture != undefined) {
+    material.map = texture;
   }
+  estrella = new THREE.Mesh(geometry, material);
+  scene.add(estrella);
 }
 
-function Estrella(rad, col) {
-    let geometry = new THREE.SphereGeometry(rad, 30, 30);
-    let material = new THREE.MeshBasicMaterial({ color: col });
-    estrella = new THREE.Mesh(geometry, material);
-    scene.add(estrella);
-}
-
-
-function Planeta(px, py, pz, radio, col, vel, f1, f2) {
-  let geometry = new THREE.SphereGeometry(radio, 30, 30);
-  //Material con o sin relleno
-  let material = new THREE.MeshBasicMaterial({
-    color: col,
-  });
-
-  let planeta = new THREE.Mesh(geometry, material);
-  planeta.userData.dist = Math.sqrt(Math.pow(px, 2) + Math.pow(py, 2));
+function Planeta(
+  parent,
+  radio,
+  dist,
+  vel,
+  col,
+  f1,
+  f2,
+  texture = undefined,
+  texbump = undefined,
+  texspec = undefined,
+  texalpha = undefined,
+) {
+  let geom = new THREE.SphereGeometry(radio, 30, 30);
+  let mat = new THREE.MeshPhongMaterial({ color: col });
+  if (texture != undefined) {
+    mat.map = texture;
+  }
+  // Textura
+  if (texture != undefined) {
+    mat.map = texture;
+  }
+  // Rugosidad
+  if (texbump != undefined) {
+    mat.bumpMap = texbump;
+    mat.bumpScale = 1;
+  }
+  // Especular
+  if (texspec != undefined) {
+    mat.specularMap = texspec;
+    mat.specular = new THREE.Color("orange");
+  }
+  if (texalpha != undefined) {
+    //Con mapa de transparencia
+    mat.alphaMap = texalpha;
+    mat.transparent = true;
+    mat.side = THREE.DoubleSide;
+    mat.opacity = 1.0;
+  }
+  let planeta = new THREE.Mesh(geom, mat);
+  planeta.castShadow = true;
+  planeta.receiveShadow = true;
+  planeta.userData.dist = dist;
   planeta.userData.speed = vel;
   planeta.userData.f1 = f1;
   planeta.userData.f2 = f2;
-  // Calcular el ángulo inicial del planeta para poder ponerlo exactamente donde
-  // hemos hecho click
-  planeta.userData.angle = Math.atan2(
-    py / (f2 * planeta.userData.dist),
-    px / (f1 * planeta.userData.dist)
-  );
-  planeta.position.set(px, py, pz);
-  scene.add(planeta);
+  planeta.userData.angle = 0;
+
   Planetas.push(planeta);
-  // Dibujar órbitas
-  let curve = new THREE.EllipseCurve(0, 0, planeta.userData.dist * f1, planeta.userData.dist * f2);
-  let points = curve.getPoints(50);
+  scene.add(planeta);
+
+  planeta.userData.angle = Math.atan2(
+    planeta.position.z / (f2 * planeta.userData.dist),
+    planeta.position.x / (f1 * planeta.userData.dist)
+  );
+
+  //Dibuja trayectoria, con
+  let curve = new THREE.EllipseCurve(
+    0,
+    0, // centro
+    dist * f1,
+    dist * f2 // radios elipse
+  );
+  //Crea geometría
+  let points = curve.getPoints(1000);
   let geome = new THREE.BufferGeometry().setFromPoints(points);
-  let mate = new THREE.LineBasicMaterial({ color: 0xffffff });
+  let mate = new THREE.LineBasicMaterial({ color: 0xa9a9a9 });
+  // Objeto
   let orbita = new THREE.Line(geome, mate);
+  orbita.rotation.x = Math.PI/2;
   scene.add(orbita);
 }
 
-function Luna(planeta, radio, dist, vel, col, angle) {
+function Luna(planeta, radio, dist, vel, col, angle, texture = undefined) {
   var pivote = new THREE.Object3D();
-  pivote.rotation.x = angle;
+  pivote.rotation.y = angle;
   planeta.add(pivote);
-  var geom = new THREE.SphereGeometry(radio, 10, 10);
-  var mat = new THREE.MeshBasicMaterial({ color: col });
+  var geom = new THREE.SphereGeometry(radio, 30, 30);
+  var mat = new THREE.MeshPhongMaterial({ color: col });
+  if (texture != undefined) {
+    mat.map = texture;
+  }
   var luna = new THREE.Mesh(geom, mat);
+  luna.receiveShadow = true;
+  luna.castShadow = true;
   luna.userData.dist = dist;
   luna.userData.speed = vel;
 
@@ -156,24 +396,39 @@ function Luna(planeta, radio, dist, vel, col, angle) {
   pivote.add(luna);
 }
 
+function Asteroide() {}
+
 //Bucle de animación
 function animate() {
-  let last_timestamp = timestamp
-  timestamp = (Date.now() - t0) * accglobal;
-  // El tiempo que ha pasado desde el último frame
-  let time_passed = timestamp - last_timestamp;
 
   requestAnimationFrame(animate);
 
+  if (!pause) {
+    timestamp = (Date.now() - offset - t0) * accglobal;
+  }
+
+  switch(mode) {
+    case 1:
+      let t1 = new Date();
+      let secs = (t1 - t0) / 1000;
+      flyControls.update(1 * secs);
+      spaceship.position.set(camera.position.x-0.35, camera.position.y-0.5, camera.position.z+1.5);
+      break;
+    case 2:
+      camera.position.set(Planetas[2].position.x, Planetas[2].position.y+2, Planetas[2].position.z-4);
+      camera.lookAt(Planetas[2].position.x, Planetas[2].position.y, Planetas[2].position.z);
+      camcontrols.enabled = false;
+      flyControls.enabled = false;
+  }
+
   // Modifica rotación de todos los objetos
   for (let object of Planetas) {
-    object.userData.angle += object.userData.speed * time_passed;
     object.position.x =
-      Math.cos(object.userData.angle) *
+      Math.cos(object.userData.speed * speedConfig * timestamp) *
       object.userData.f1 *
       object.userData.dist;
-    object.position.y =
-      Math.sin(object.userData.angle) *
+    object.position.z =
+      Math.sin(object.userData.speed * speedConfig * timestamp) *
       object.userData.f2 *
       object.userData.dist;
   }
@@ -182,9 +437,20 @@ function animate() {
   for (let object of Lunas) {
     object.position.x =
       Math.cos(timestamp * object.userData.speed) * object.userData.dist;
-    object.position.y =
+    object.position.z =
       Math.sin(timestamp * object.userData.speed) * object.userData.dist;
   }
 
+  let earth = Planetas[2]
+
+  if (mode != 1 && spaceship != undefined) {
+    spaceship.position.x = Math.cos(earth.userData.speed * speedConfig * timestamp * (-1.5)) *
+    earth.userData.f1 * earth.userData.dist; 
+    spaceship.position.z = Math.sin(earth.userData.speed * speedConfig * timestamp * (-1.5)) *
+    earth.userData.f1 * earth.userData.dist;
+    spaceship.position.y = 3;
+  }
+
   renderer.render(scene, camera);
+  renderer.outputColorSpace = THREE.SRGBColorSpace;
 }
